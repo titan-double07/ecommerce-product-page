25c89beb9dc3f26aff6b4dcaa9404aa0c7b9061b
titanwarlord007 <chisomokereke1999@gmail.com>
2023-04-02 00:35:40 +0100

    ...

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,7 +4,7 @@ import ProductImages from './ProductImages'
 
 export default function Product() {
   return (
-    <div className='max-w-screen-lg lg:grid grid-cols-2 mx-auto items-center mt-20 '>
+    <div className='max-w-screen-lg lg:grid grid-cols-2 mx-auto items-center mt-[4.5em] lg:mt-36 '>
       <ProductImages />
       <ProductContent/>
     </div>
